9c71f32fa92223b38f4cbcf5eb8b34dcf9384802
Erik <eriktr22@gmail.com>
2020-02-13 15:02:05 -0600

    Create framework for Todo page
    Added basic todoDatabase and todoController, functionality has yet to be added. Also initialized them in the Server class

---
+++ server/src/main/java/umm3601/todos/TodoController.java
@@ -0,0 +1,10 @@
+package umm3601.todos;
+
+public class TodoController {
+
+  private TodoDatabase database;
+
+  public TodoController(TodoDatabase database) {
+    this.database = database;
+  }
+}
